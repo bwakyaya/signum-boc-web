2350a4f81cd4b7af2357d0e311d563bc765d9281
bwakyaya <ben.wak@yahoo.com>
2022-06-21 17:55:18 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="login-panel">
+        <div className='login-logo'>
+          <img src='signum-logo.png' height="100px" width="130px"></img>
+        </div>
+        <div className='username-section'>
+          <input type={'text'} placeholder='Username' className='username-field'></input>
+        </div>
+        <div className='password-section'>
+          <input type={'password'} placeholder='Password' className='password-field'></input>
+        </div>
+        <div className='button-section'>
+          <button className="login-button">Login</button>
+        </div>
+        <div className='account-options'>
+          <a className='sign-up-label' href='https://www.signumadvocates.com/' target="_blank">Sign Up</a>
+          <a className='forgot-password-label' href='https://www.signumadvocates.com/' target="_blank">Forgot Password?</a>
+        </div>
+      </div>
     </div>
   );
 }
