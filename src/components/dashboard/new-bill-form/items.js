dfa74b10f5013a30546e81af36d2e14bc9a84079
bwakyaya <ben.wak@yahoo.com>
2022-07-13 18:06:59 +0300

    Commit with initial work on the items page

--- src/components/dashboard/new-bill-form/items.js
+++ src/components/dashboard/new-bill-form/items.js
@@ -1,10 +1,17 @@
-import "./items.css"
-import React from 'react'
+// import "./items.css"
+import React, {useState} from 'react'
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+import ItemsTable from "./table-items";
 
 export default function CostItems(){
+    const [isOpen, setIsOpen] = useState(false);
+    const togglePopup = () => {
+        setIsOpen(!isOpen);
+      };
     return(
     <div>
-        
+        <ItemsTable />
+        <img src='add-button.png' width="30px" height="30px" alt="Add Button"></img>
     </div>
     );
 }
