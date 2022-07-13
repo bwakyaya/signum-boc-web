aaa8e51bf1d484b300f9271a2463e83a8567daa6
bwakyaya <ben.wak@yahoo.com>
2022-07-13 14:33:48 +0300

    Minor Commit

--- src/components/dashboard/new-bill-form/items.js
+++ src/components/dashboard/new-bill-form/items.js
@@ -1,11 +1,10 @@
 import "./items.css"
-import ScheduleTree from "./schedule-tree"
 import React from 'react'
 
 export default function CostItems(){
     return(
     <div>
-        <ScheduleTree />
+        
     </div>
     );
 }
