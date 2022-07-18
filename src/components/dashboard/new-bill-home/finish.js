2a6fb3e138cc4531a2919fb68fe44bdf96e37753
bwakyaya <ben.wak@yahoo.com>
2022-07-18 21:44:48 +0300

    Commit with proper form setup. pending css

--- src/components/dashboard/new-bill-home/finish.js
+++ src/components/dashboard/new-bill-home/finish.js
@@ -6,14 +6,14 @@ class Finish extends Component {
   render() {
     return (
       <div className="save-button-div">
-        <input
+        {/* <input
           type="button"
           value="Save"
           className="save-button"
           onClick={() => {
             alert("Hey guys, I have just been ");
           }}
-        ></input>
+        ></input> */}
         <ScheduleTree />
       </div>
     );
--- src/components/dashboard/new-bill-home/index.js
+++ src/components/dashboard/new-bill-home/index.js
@@ -1,16 +1,107 @@
-import React from "react";
-// import FormTabs from "./form-tabs";
+import React, { useState } from "react";
+import { Stepper } from "react-form-stepper";
 import SummaryPage from "./summary";
 import CostItems from "./items";
 import Disbursements from "./disbursements";
+import Finish from "./finish";
 
 export default function NewBillForm() {
+  const [activeStep, setActiveStep] = useState(0);
+  function switchFormParts(step) {
+    let Component;
+    switch (step) {
+      case 0:
+        Component = <SummaryPage />;
+        break;
+      case 1:
+        Component = <CostItems />;
+        break;
+      case 2:
+        Component = <Disbursements />;
+        break;
+      case 3:
+        Component = <Finish />;
+        break;
+      default:
+    }
+    return Component;
+  }
+  const ConnectorStyleProps = {
+    disabledColor: "#bdbdbd",
+    activeColor: "#4494df",
+    completedColor: "#04203A",
+    size: 1,
+    stepSize: "1.5em",
+    style: "solid",
+  };
+  const StepStyleDTO = {
+    activeBgColor: "#4494df",
+    activeTextColor: "#ffffff",
+    completedBgColor: "#04203A",
+    completedTextColor: "#ffffff",
+    inactiveBgColor: "#e0e0e0",
+    inactiveTextColor: "#ffffff",
+    size: "2em",
+    circleFontSize: "1rem",
+    borderRadius: "50%",
+  };
+  function nextHidden(x) {
+    let hiddenValue;
+    if (x === 3) {
+      hiddenValue = true;
+    } else {
+      hiddenValue = false;
+    }
+    return hiddenValue;
+  }
+  function previousHidden(y) {
+    let hiddenValue;
+    if (y === 0) {
+      hiddenValue = true;
+    } else {
+      hiddenValue = false;
+    }
+    return hiddenValue;
+  }
   return (
     <div className="body">
-      <SummaryPage />
-      <CostItems />
-      <label>Disbursements</label>
-      <Disbursements />
+      <Stepper
+        className="form-steps"
+        steps={[
+          { label: "Summary Detail" },
+          { label: "Primary Cost Items" },
+          { label: "Disbursements" },
+          { label: "Finish" },
+        ]}
+        activeStep={activeStep}
+        connectorStateColors={true}
+        hideConnectors={false}
+        connectorStyleConfig={ConnectorStyleProps}
+        styleConfig={StepStyleDTO}
+      />
+      {switchFormParts(activeStep)}
+      <div>
+        <button
+          hidden={previousHidden(activeStep)}
+          onClick={() => {
+            if (activeStep > 0) {
+              setActiveStep(activeStep - 1);
+            }
+          }}
+        >
+          Previous
+        </button>
+        <button
+          hidden={nextHidden(activeStep)}
+          onClick={() => {
+            if (activeStep < 3) {
+              setActiveStep(activeStep + 1);
+            }
+          }}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
