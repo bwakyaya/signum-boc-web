43ea19bfa09daec1df35e983939964b739b4100d
bwakyaya <ben.wak@yahoo.com>
2022-07-29 00:51:54 +0300

    Commit with upload function for bill

--- src/components/dashboard/new-bill-home/finish.js
+++ src/components/dashboard/new-bill-home/finish.js
@@ -2,11 +2,36 @@ import React from "react";
 import "./finish.css";
 import Button from "@mui/material/Button";
 import SaveIcon from "@mui/icons-material/Save";
+import axios from "axios";
+
+async function handleBillUpload(summary, items, disbursements) {
+  const url = "http://localhost:5000/authenticate";
+  const formData = new FormData();
+  formData.append("summary", summary);
+  formData.append("items", items);
+  formData.append("disbursements", disbursements);
+  const config = {
+    headers: {
+      "content-type": "multipart/form-data",
+    },
+  };
+  return await axios
+    .post(url, formData, config)
+    .then((data) => data)
+    .then((Data) => {
+      if (Data.data === "Success") {
+        alert("Bill saved successfully");
+      } else {
+        alert("Oops! Problem occurred while uploading bill");
+      }
+    });
+}
 
 export default function Finish(props) {
   const summary = props.summary;
-  // const items = props.items;
-  // const disbursements = props.disbursements;
+  const items = props.items;
+  const disbursements = props.disbursements;
+
   return (
     <div className="save-button-div">
       <Button
@@ -14,7 +39,7 @@ export default function Finish(props) {
         variant="contained"
         startIcon={<SaveIcon />}
         onClick={() => {
-          alert(summary.date);
+          handleBillUpload(summary, items, disbursements);
         }}
       >
         Complete
