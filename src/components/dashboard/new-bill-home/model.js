b1960d675b4af18b57b61be708bc2e051ad9b0e4
bwakyaya <ben.wak@yahoo.com>
2022-08-08 10:42:24 +0300

    Commit with Model

---
+++ src/components/dashboard/new-bill-home/model.js
@@ -0,0 +1,306 @@
+export function s1s1p1(amount) {
+  let Amount = null;
+  switch (amount) {
+    case amount < 10000000:
+      Amount = 0.05 * amount;
+      break;
+    case (amount = 10000000):
+      Amount = 500000;
+      break;
+    case amount > 10000000 && amount <= 20000000:
+      Amount = 500000 + 0.03 * (amount - 10000000);
+      break;
+    case amount > 20000000:
+      Amount = 500000 + 300000 + 0.02 * (amount - 20000000);
+      break;
+    default:
+      break;
+  }
+  return Amount;
+}
+export function s1s1p2(amount) {
+  let Amount = null;
+  let itemOneFee = s1s1p1(amount);
+  Amount = 0.05 * itemOneFee;
+  return Amount;
+}
+export function s1s1p3(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s1s2p1(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s1s2p2(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s1s2p3(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s1s2p4(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s1s2p5(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s1s2p6(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s1s2p7(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s1s2p8(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s1s2p9(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s1s2p10(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s1s2p11(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s1s2p12(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s1s2p13(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s2s1p1(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s2s1p2(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s2s2p1(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s2s2p2(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s3s1p1(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s3s1p2(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s3s1p3(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s3s1p4(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s3s2p1(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s3s2p2(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s3s3p1(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s3s3p2(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s4s1p1(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s4s1p2(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s4s1p3(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s4s1p4(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s4s1p5(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s4s1p6(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s4s1p7(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s4s1p8(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s4s1p9(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s4s1p10(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s4s2p1(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s4s2p2(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s4s2p3(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s4s2p4(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s4s2p5(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s4s2p6(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s4s2p7(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s4s2p8(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s4s2p9(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s4s2p10(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s4s2p11(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s4s2p12(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s5p1(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s5p2(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s5p3(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s5p4(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s5p5(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s5p6(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s5p7(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s5p8(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s5p9(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s5p10(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s6p1(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s6p2(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s6p3(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s6p4(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s6p5(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s6p6(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s6p7(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s6p8(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s6p9(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s6p10(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s6p11(amount) {
+  let Amount = null;
+  return Amount;
+}
+export function s6p12(amount) {
+  let Amount = null;
+  return Amount;
+}
