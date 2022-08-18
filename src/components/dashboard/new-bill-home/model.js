502be5ac139047cadc071ed19b9847339732b451
bwakyaya <ben.wak@yahoo.com>
2022-08-18 14:53:07 +0300

    Commit with all model logic

--- src/components/dashboard/new-bill-home/model.js
+++ src/components/dashboard/new-bill-home/model.js
@@ -75,41 +75,75 @@ export function s1s2p7(amount) {
   return s1s2p1(amount) * 0.25;
 }
 //Needs Clarification
-export function s1s2p8(amount) {
+export function s1s2p8(prescribedFeePerSecurity, numberOfSecurities) {
   let Amount = null;
-  alert("Needs Clarification");
+  switch (numberOfSecurities) {
+    case (numberOfSecurities = 1):
+      Amount = prescribedFeePerSecurity;
+      break;
+    case (numberOfSecurities = 2):
+      Amount = prescribedFeePerSecurity + 0.25 * prescribedFeePerSecurity;
+      break;
+    case numberOfSecurities > 2:
+      Amount =
+        prescribedFeePerSecurity +
+        0.25 * prescribedFeePerSecurity +
+        0.1 * prescribedFeePerSecurity * (numberOfSecurities - 2);
+      break;
+    default:
+      break;
+  }
   return Amount;
 }
-//Needs Clarification
-export function s1s2p9(amount) {
+//Needs Clarification - Is the first immovable property free of charge?
+export function s1s2p9(numberOfImmovableProperty, prescribedFee) {
   let Amount = null;
-  alert("Needs Clarification");
+  switch (numberOfImmovableProperty) {
+    case (numberOfImmovableProperty = 2):
+      Amount = 0.1 * prescribedFee;
+      break;
+    case numberOfImmovableProperty < 2:
+      Amount = prescribedFee + 0.05 * (numberOfImmovableProperty - 2);
+      break;
+    default:
+      break;
+  }
   return Amount;
 }
-//Needs Clarification
-export function s1s2p10(amount) {
+//Needs Clarification - The meaning of the feel shall be divided equally
+export function s1s2p10(prescribedFeePerChargee, numberOfChargees) {
   let Amount = null;
-  alert("Needs Clarification");
+  switch (numberOfChargees) {
+    case (numberOfChargees = 1):
+      Amount = prescribedFeePerChargee;
+      break;
+    case numberOfChargees > 1:
+      Amount =
+        prescribedFeePerChargee +
+        0.05 * prescribedFeePerChargee(numberOfChargees - 1);
+      break;
+    default:
+      break;
+  }
   return Amount;
 }
-//Needs Clarification
-export function s1s2p11(amount) {
-  let Amount = null;
-  alert("Needs Clarification");
-  return Amount;
-}
-//Needs Clarification
-export function s1s2p12(amount) {
-  let Amount = null;
-  alert("Needs Clarification");
-  return Amount;
-}
-//Needs Clarification
-export function s1s2p13(amount) {
-  let Amount = null;
-  alert("Needs Clarification");
-  return Amount;
-}
+// //Needs Clarification - Is this a paragraph of its own or it is a condition for the above items
+// export function s1s2p11(numberOfItemsNotHandled) {
+//   let Amount = null;
+//   return Amount;
+// }
+// //Needs Clarification - Is this a paragraph of its own or it is a condition for the above items
+// export function s1s2p12(amount) {
+//   let Amount = null;
+//   alert("Needs Clarification");
+//   return Amount;
+// }
+// //Needs Clarification - Is this a paragraph of its own or it is a condition for the above items
+// export function s1s2p13(amount) {
+//   let Amount = null;
+//   alert("Needs Clarification");
+//   return Amount;
+// }
 //Schedule 2
 export function s2s1p1(amount) {
   let Amount = null;
@@ -238,6 +272,7 @@ export function s3s3p1(amount) {
 // }
 
 //Schedule 4
+//Add more items to the tree
 export function s4s1p1x1() {
   return 1000000;
 }
@@ -372,6 +407,7 @@ export function s4s2p1(amount) {
   }
   return 3000000;
 }
+//add more items to the tree
 export function s4s2p2x1() {
   return 2000000;
 }
@@ -411,6 +447,7 @@ export function s4s2p8() {
 export function s4s2p9() {
   return 300000;
 }
+//add more items to the tree
 export function s4s2p10x1() {
   return 100000;
 }
@@ -563,51 +600,346 @@ export function s5p10(amount) {
   }
   return Amount;
 }
-export function s6p1(amount) {
+//Schedule Six
+export function s6p1x1(amount) {
   let Amount = null;
+  switch (amount) {
+    case amount <= 2000000:
+      Amount = 0.15 * amount;
+      break;
+    case amount > 2000000 && amount <= 5000000:
+      Amount = 0.15 * 2000000 + 0.14 * (amount - 2000000);
+      break;
+    case amount > 5000000 && amount <= 10000000:
+      Amount = 0.15 * 2000000 + 0.14 * 3000000 + 0.12 * (amount - 5000000);
+      break;
+    case amount > 10000000 && amount <= 20000000:
+      Amount =
+        0.15 * 2000000 +
+        0.14 * 3000000 +
+        0.12 * 5000000 +
+        0.1 * (amount - 10000000);
+      break;
+    case amount > 20000000 && amount <= 50000000:
+      Amount =
+        0.15 * 2000000 +
+        0.14 * 3000000 +
+        0.12 * 5000000 +
+        0.1 * 10000000 +
+        0.08 * (amount - 20000000);
+      break;
+    case amount > 50000000 && amount <= 100000000:
+      Amount =
+        0.15 * 2000000 +
+        0.14 * 3000000 +
+        0.12 * 5000000 +
+        0.1 * 10000000 +
+        0.08 * 30000000 +
+        0.05 * (amount - 50000000);
+      break;
+    case amount > 100000000:
+      Amount =
+        0.15 * 2000000 +
+        0.14 * 3000000 +
+        0.12 * 5000000 +
+        0.1 * 10000000 +
+        0.08 * 30000000 +
+        0.05 * 50000000 +
+        0.02(amount - 100000000);
+      break;
+    default:
+      break;
+  }
   return Amount;
 }
-export function s6p2(amount) {
-  let Amount = null;
+export function s6p1x2(amount) {
+  let Amount = 0.65 * s6p1x1(amount);
   return Amount;
 }
-export function s6p3(amount) {
-  let Amount = null;
+export function s6p1x3(amount) {
+  let Amount = 0.75 * s6p1x1(amount);
   return Amount;
 }
+export function s6p1x4(amount) {
+  let Amount = 0.85 * s6p1x1(amount);
+  return Amount;
+}
+//Add more items to the tree
+export function s6p2x1(amount) {
+  let Amount = null;
+  switch (amount) {
+    case amount <= 2000000:
+      Amount = 0.15 * amount;
+      break;
+    case amount > 2000000 && amount <= 5000000:
+      Amount = 0.15 * 2000000 + 0.14 * (amount - 2000000);
+      break;
+    case amount > 5000000 && amount <= 10000000:
+      Amount = 0.15 * 2000000 + 0.14 * 3000000 + 0.12 * (amount - 5000000);
+      break;
+    case amount > 10000000 && amount <= 20000000:
+      Amount =
+        0.15 * 2000000 +
+        0.14 * 3000000 +
+        0.12 * 5000000 +
+        0.1 * (amount - 10000000);
+      break;
+    case amount > 20000000 && amount <= 50000000:
+      Amount =
+        0.15 * 2000000 +
+        0.14 * 3000000 +
+        0.12 * 5000000 +
+        0.1 * 10000000 +
+        0.08 * (amount - 20000000);
+      break;
+    case amount > 50000000 && amount <= 100000000:
+      Amount =
+        0.15 * 2000000 +
+        0.14 * 3000000 +
+        0.12 * 5000000 +
+        0.1 * 10000000 +
+        0.08 * 30000000 +
+        0.05 * (amount - 50000000);
+      break;
+    case amount > 100000000:
+      Amount =
+        0.15 * 2000000 +
+        0.14 * 3000000 +
+        0.12 * 5000000 +
+        0.1 * 10000000 +
+        0.08 * 30000000 +
+        0.05 * 50000000 +
+        0.02(amount - 100000000);
+      break;
+    default:
+      break;
+  }
+  return Amount;
+}
+export function s6p2x2(amount) {
+  let Amount = 0.65 * s6p2x1(amount);
+  return Amount;
+}
+export function s6p2x3(amount) {
+  let Amount = 0.75 * s6p2x1(amount);
+  return Amount;
+}
+export function s6p2x4(amount) {
+  let Amount = 0.85 * s6p2x1(amount);
+  return Amount;
+}
+//add more tree children
+export function s6p3x1a(amount) {
+  if (amount > 1000000) {
+    return amount;
+  }
+  return 1000000;
+}
+export function s6p3x1b(amount) {
+  if (amount > 2000000) {
+    return amount;
+  }
+  return 2000000;
+}
+export function s6p3x2a(amount) {
+  if (amount > 1000000) {
+    return amount;
+  }
+  return 1000000;
+}
+export function s6p3x2b(amount) {
+  if (amount > 1500000) {
+    return amount;
+  }
+  return 1500000;
+}
+
 export function s6p4(amount) {
-  let Amount = null;
+  if (amount > 500000) {
+    return amount;
+  }
+  return 500000;
+}
+export function s6p5a(grossCapitalValue) {
+  let Amount = 0.01 * grossCapitalValue;
+  if (Amount > 1000000) {
+    return Amount;
+  }
+  return 1000000;
+}
+export function s6p5b(grossCapitalValue) {
+  let Amount = 0.02 * grossCapitalValue;
+  if (Amount > 1000000) {
+    return Amount;
+  }
+  return 1000000;
+}
+//Need Clarification : add more tree children
+export function s6p5c1(amount) {
+  let Amount = (4 / 5) * s6p1x1(amount);
   return Amount;
 }
-export function s6p5(amount) {
-  let Amount = null;
+export function s6p5c2(amount) {
+  let Amount = (4 / 5) * s6p1x2(amount);
   return Amount;
 }
-export function s6p6(amount) {
+export function s6p5c3(amount) {
+  let Amount = (4 / 5) * s6p1x3(amount);
+  return Amount;
+}
+export function s6p5c4(amount) {
+  let Amount = (4 / 5) * s6p1x4(amount);
+  return Amount;
+}
+//add more tree children
+export function s6p5d1a(amount, negotiatedAmount) {
+  let tempAmount = 2 * s6p3x1a(amount);
+  if (negotiatedAmount > tempAmount) {
+    return negotiatedAmount;
+  }
+  return tempAmount;
+}
+export function s6p5d1b(amount, negotiatedAmount) {
+  let tempAmount = 2 * s6p3x1b(amount);
+  if (negotiatedAmount > tempAmount) {
+    return negotiatedAmount;
+  }
+  return tempAmount;
+}
+export function s6p5d2a(amount, negotiatedAmount) {
+  let tempAmount = 2 * s6p3x2a(amount);
+  if (negotiatedAmount > tempAmount) {
+    return negotiatedAmount;
+  }
+  return tempAmount;
+}
+export function s6p5d2b(amount, negotiatedAmount) {
+  let tempAmount = 2 * s6p3x2b(amount);
+  if (negotiatedAmount > tempAmount) {
+    return negotiatedAmount;
+  }
+  return tempAmount;
+}
+export function s6p5e(amount) {
+  if (amount > 500000) {
+    return amount;
+  }
+  return 500000;
+}
+export function s6p5f(amount) {
+  if (amount > 500000) {
+    return amount;
+  }
+  return 500000;
+}
+export function s6p5g(amount) {
+  if (amount > 500000) {
+    return amount;
+  }
+  return 500000;
+}
+export function s6p5h(amount) {
+  if (amount > 1000000) {
+    return amount;
+  }
+  return 1000000;
+}
+export function s6p6(amount, act) {
   let Amount = null;
+  switch (act) {
+    case "parliamentary":
+      if (amount > 10000000) {
+        Amount = amount;
+      }
+      Amount = 10000000;
+      break;
+    case "local-governments":
+      if (amount > 5000000) {
+        Amount = amount;
+      }
+      Amount = 5000000;
+      break;
+    default:
+      break;
+  }
   return Amount;
 }
 export function s6p7(amount) {
-  let Amount = null;
+  if (amount > 3000000) {
+    return amount;
+  }
+  return 3000000;
+}
+//add more children to the tree
+export function s6p8x1(amount) {
+  if (amount > 2000000) {
+    return amount;
+  }
+  return 2000000;
+}
+export function s6p8x2(amount) {
+  if (amount > 500000) {
+    return amount;
+  }
+  return 500000;
+}
+//add more children
+export function s6p9x1(amount) {
+  if (amount > 2000000) {
+    return amount;
+  }
+  return 2000000;
+}
+export function s6p9x2(amount) {
+  if (amount > 300000) {
+    return amount;
+  }
+  return 300000;
+}
+//Extra Children needed
+export function s6p10x1(numberOfCopies) {
+  let Amount = 300000 + 50000 * numberOfCopies;
   return Amount;
 }
-export function s6p8(amount) {
-  let Amount = null;
+export function s6p10x2(numberOfCopies) {
+  let Amount = 200000 + 30000 * numberOfCopies;
   return Amount;
 }
-export function s6p9(amount) {
-  let Amount = null;
+export function s6p10x3(numberOfCopies) {
+  let Amount = 50000 + 20000 * numberOfCopies;
   return Amount;
 }
-export function s6p10(amount) {
-  let Amount = null;
+export function s6p10x4(numberOfCopies) {
+  let Amount = 300000 + 50000 * numberOfCopies;
   return Amount;
 }
-export function s6p11(amount) {
-  let Amount = null;
+export function s6p10x5(numberOfCopies) {
+  let Amount = 200000 + 50000 * numberOfCopies;
   return Amount;
 }
-export function s6p12(amount) {
-  let Amount = null;
+//extra children needed
+export function s6p11x1() {
+  let Amount = 100000;
+  return Amount;
+}
+export function s6p11x2() {
+  let Amount = 20000;
+  return Amount;
+}
+//extra children needed
+export function s6p12x1(numberOfHours) {
+  let Amount = numberOfHours * 100000;
+  return Amount;
+}
+export function s6p12x2a(numberOfHours) {
+  let Amount = numberOfHours * 100000;
+  return Amount;
+}
+export function s6p12x2b(numberOfHours) {
+  let Amount = numberOfHours * 20000;
+  return Amount;
+}
+export function s6p12x3(numberOfHours) {
+  let Amount = numberOfHours * 100000;
   return Amount;
 }
