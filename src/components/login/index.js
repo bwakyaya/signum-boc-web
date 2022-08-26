d046169543feea55875b99d3d4e0685ed3e9568c
bwakyaya <ben.wak@yahoo.com>
2022-08-26 12:28:46 +0300

    Commit with new login-page design.

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -28,61 +28,64 @@ export default function Login(props) {
     <div className="login-page">
       <div className="login-panel">
         <div className="login-logo">
-          {/* <img
+          <img
+            className="logo"
             src="signum-logo.png"
             height="100px"
             width="130px"
             alt="logo-here"
-          ></img> */}
+          ></img>
+        </div>
+        <div className="login-options">
           <h2 className="system-name">Bill of Costs</h2>
-        </div>
-        <div className="username-section">
-          <input
-            type={"text"}
-            placeholder="Username"
-            className="login-field"
-            id="usernameField"
-          ></input>
-        </div>
-        <div className="password-section">
-          <input
-            type={"password"}
-            placeholder="Password"
-            className="login-field"
-            id="passwordField"
-          ></input>
-        </div>
-        <div className="button-section">
-          <button
-            className="login-button"
-            onClick={() => {
-              const userName = document.getElementById("usernameField").value;
-              const passWord = document.getElementById("passwordField").value;
-              handleLogin(userName, passWord);
-              props.setToken("dkjfngkdsfjngj878435kjnbsf984");
-              <App />;
-            }}
-          >
-            LOGIN
-          </button>
-        </div>
-        <div className="account-options">
-          <a
-            className="sign-up-label"
-            href="https://www.signumadvocates.com/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Sign Up
-          </a>
-          <a
-            className="forgot-password-label"
-            href="https://www.signumadvocates.com/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Forgot Password?
-          </a>
+          <div className="username-section">
+            <input
+              type={"text"}
+              placeholder="Username"
+              className="login-field"
+              id="usernameField"
+            ></input>
+          </div>
+          <div className="password-section">
+            <input
+              type={"password"}
+              placeholder="Password"
+              className="login-field"
+              id="passwordField"
+            ></input>
+          </div>
+          <div className="button-section">
+            <button
+              className="login-button"
+              onClick={() => {
+                const userName = document.getElementById("usernameField").value;
+                const passWord = document.getElementById("passwordField").value;
+                handleLogin(userName, passWord);
+                props.setToken("dkjfngkdsfjngj878435kjnbsf984");
+                <App />;
+              }}
+            >
+              LOGIN
+            </button>
+          </div>
+          <div className="account-options">
+            <a
+              className="sign-up-label"
+              href="https://www.signumadvocates.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Sign Up
+            </a>
+            <a
+              className="forgot-password-label"
+              href="https://www.signumadvocates.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Forgot Password?
+            </a>
+          </div>
         </div>
       </div>
     </div>
